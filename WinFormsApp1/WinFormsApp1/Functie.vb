1edef31323165033231d7c115e4bbe69e43653f4
owencorstens <deco-it@outlook.com>
2023-01-26 16:06:30 +0100

    more template changes

--- WinFormsApp1/WinFormsApp1/Functie.vb
+++ WinFormsApp1/WinFormsApp1/Functie.vb
@@ -1,7 +1,109 @@
 ﻿Public Class Functie
+    Dim nummer As Integer
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim myForm As New Form1
         myForm.Show()
         Me.Close()
+    End Sub
+
+    Private Sub Functie_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        nummer = InputBox("welke oefening testen wij?")
+        selectOef(nummer)
+    End Sub
+
+    Private Sub selectOef(nummer)
+        Select Case nummer
+            Case 1
+                oefening1()
+            Case 2
+                oefening2()
+            Case 3
+                oefening3()
+            Case 4
+                oefening4()
+            Case 5
+                oefening5()
+            Case 6
+                oefening6()
+            Case 7
+                oefening7()
+            Case 8
+                oefening8()
+            Case 9
+                oefening9()
+            Case 10
+                oefening10()
+            Case 11
+                oefening11()
+            Case 12
+                oefening12()
+            Case 13
+                oefening13()
+            Case 14
+                oefening14()
+            Case 15
+                oefening15()
+        End Select
+    End Sub
+
+    Private Sub oefening1()
+
+    End Sub
+
+    Private Sub oefening2()
+
+    End Sub
+
+    Private Sub oefening3()
+
+
+    End Sub
+
+    Private Sub oefening4()
+
+    End Sub
+
+    Private Sub oefening5()
+
+    End Sub
+
+    Private Sub oefening6()
+
+    End Sub
+
+    Private Sub oefening7()
+
+    End Sub
+
+    Private Sub oefening8()
+
+    End Sub
+
+    Private Sub oefening9()
+
+    End Sub
+
+    Private Sub oefening10()
+
+    End Sub
+
+    Private Sub oefening11()
+
+    End Sub
+
+    Private Sub oefening12()
+
+    End Sub
+
+    Private Sub oefening13()
+
+    End Sub
+
+    Private Sub oefening14()
+
+    End Sub
+
+    Private Sub oefening15()
+
     End Sub
 End Class
--- WinFormsApp1/WinFormsApp1/Selectie.Designer.vb
+++ WinFormsApp1/WinFormsApp1/Selectie.Designer.vb
@@ -23,6 +23,8 @@ Partial Class Selectie
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
@@ -34,17 +36,40 @@ Partial Class Selectie
         Me.Button1.Text = "Back"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(200, 135)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "resultaat"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(200, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(208, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Het resultaat van de oefening:"
+        '
         'Selectie
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Selectie"
         Me.Text = "Selectie"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class

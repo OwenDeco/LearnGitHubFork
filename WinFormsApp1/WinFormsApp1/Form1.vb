7cf4166a2ef0ac1d64703b7a55b6c2bd41834910
owencorstens <deco-it@outlook.com>
2023-01-26 13:18:47 +0100

    Prep oefeningen

--- WinFormsApp1/WinFormsApp1/Form1.vb
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -1,3 +1,29 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim myForm As New Selectie
+        myForm.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim myForm As New Iteratie
+        myForm.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim myForm As New Array
+        myForm.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim myForm As New Functie
+        myForm.Show()
+        Me.Close()
+    End Sub
 End Class
---
+++ WinFormsApp1/WinFormsApp1/Functie.vb
@@ -0,0 +1,7 @@
+﻿Public Class Functie
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim myForm As New Form1
+        myForm.Show()
+        Me.Close()
+    End Sub
+End Class
